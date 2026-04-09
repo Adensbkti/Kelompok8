de0a7d0df688c53721db479197e3dfa4bb7d8667
Adensbkti <azmalsubakti@gmail.com>
2026-04-09 16:16:33 +0700

    menambahkan file settings

---
+++ resources/views/pages/settings.blade.php
@@ -0,0 +1,9 @@
+ @extends('layouts.app')
+
+@section('content')
+<h2 class="text-2xl font-bold mb-4">Settings</h2>
+
+<div class="bg-white p-4 rounded shadow">
+    <p>Pengaturan aplikasi</p>
+</div>
+@endsection   
